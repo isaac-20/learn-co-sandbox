d4fb1641f5e28fb64c7028e61ea72c4192e0f927
Isaac Smith <isaacsmith215@aol.com>
2020-04-02 14:43:36 +0000

    Automatically backed up by Learn

---
+++ ike.rb
@@ -0,0 +1,17 @@
+def practice
+  
+  puts "hey"
+  
+end
+practice
+practice
+practice 
+def ike(name, language)
+  puts "hey #{name}! we heard you're a great #{language} programmer"
+end
+ike("Isaac","Computer")
+def print_return
+  "heyo"
+  puts "yohey"
+end
+print_return
